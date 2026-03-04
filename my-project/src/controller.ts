a4a5d7e9da4a3a3933298fb13d5455de7e58bf4a
Ashith KE <ashithke@gmail.com>
2026-03-04 10:32:51 +0530

    Configure for deployment

--- my-project/src/controller.ts
+++ my-project/src/controller.ts
@@ -3,13 +3,12 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '@prisma/client';
 
 export const pool = mysql.createPool({ 
-  host: '127.0.0.1', 
-  user: 'root',
-  password: '', 
-  database: 'bitespeed_db', 
-  port: 3306,
-  connectionLimit: 1,
-  connectTimeout: 20000 
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME, 
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false } // Required for Aiven
 });
 
 const adapter = new PrismaMariaDb(pool as any);
